e64469cf8fb621bb3bc63e6966031e0434448d3d
ctspencer2 <118775383+ctspencer2@users.noreply.github.com>
2023-10-01 13:12:07 -0700

    done

--- javascript.js
+++ javascript.js
@@ -1,13 +1,72 @@
 // 1.1 Assignment: Where do you put JavaScript?
+alert("Javascript is easy")
 
 // 1.2 Assignment: Variables
+var guyName = "john";
+var guyAge = 25;
+var guyFrom = "United Kingdom";
+var gender1 = "Male";
+
+
+console.log(guyName, guyAge, guyFrom, gender1);
+
 
 // 1.3 Assignment: Variable Declaration & Initialization
+let a = 403;
+let b = "Hawaii";
+let c = true;
+let d = false;
+let e;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
 
 // 1.4 Assignment: Complete your statements
+x = 2; y = 3;
+
+z = x + y;
+
+console.log (z);
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var q = function(){
+    alert('Statement 1');
+   alert('Statement 2');
+}
+
+console.log (q);
+
+q();
+
 
 // 1.6 Assignment: Data Types
 
+
+
+// string
+var x = "Carson";
+var y = 'Spencer';
+
+// number
+var z = 25;
+
+// boolean
+var w = true;
+
+console.log ("First Name:", x)
+console.log ("Last Name:", y)
+console.log ("Age:", z)
+console.log ("Likes Coffee:", w)
+
+
+
 // 1.7 Assignment: Practicing JavaScript basics
+var A = 4;
+var B = 3;
+var C = 5;
+var area = (B*A)/2;
+
+console.log ("area:", 6);
